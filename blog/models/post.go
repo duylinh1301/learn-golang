bac496063f4282d83269393dd0f2d8253fe36f95
Duy Linh <linhnguyen@okxe.vn>
2020-12-11 17:04:01 +0700

    add ignore

--- blog/models/post.go
+++ blog/models/post.go
@@ -1,7 +1,5 @@
 package models
 
-import "net/http"
-
 type Post struct {
 	id      int    `json:"id"`
 	title   string `json:"title"`
@@ -9,6 +7,11 @@ type Post struct {
 }
 
 // Get all posts
-func getAllPosts(w http.ResponseWriter, r *http.Request) {
-
+func (p *Post) All() Post {
+	p = &Post{
+		id:      1,
+		title:   "Lorem ipsum dolor sit amet.",
+		content: "Lonsectetur adipiscing elit. Donec facilisis dapibus suscipit. Duis dignissim eleifend justo, quis consequat lorem lacinia vel. Quisque scelerisque massa a pellentesque laoreet. In ac eleifend orci. Praesent ut nulla ac felis sagittis viverra ut in velit. Vivamus eu mauris fringilla, tempor mi et, mollis tellus. Vivamus in lorem at sem finibus laoreet in id orci. Morbi et consectetur nisl, quis ultrices ante. In malesuada sem a ex finibus, vel ullamcorper justo laoreet.",
+	}
+	return *p
 }
--- blog/route/api.go
+++ blog/route/api.go
@@ -2,6 +2,7 @@ package route
 
 import (
 	"blog/controllers/admin/dashboard"
+	postcontroller "blog/controllers/blog"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -9,14 +10,17 @@ import (
 
 func Router() {
 
-	r := mux.NewRouter()
+	r := mux.NewRouter().StrictSlash(true)
 
 	// Route Handler
-	r.HandleFunc("/api/posts", getPosts).Methods("GET")
+	r.HandleFunc("/api/posts", postcontroller.GetAllPosts).Methods("GET")
+	// http.HandleFunc("/api/posts", postcontroller.GetAllPosts)
 	// r.HandleFunc("/api/posts/{id}", getPost).Methods("GET")
 	// r.HandleFunc("/api/posts", createPost).Methods("POST")
 	// r.HandleFunc("/api/posts/{id}", updatePost).Methods("PUT")
 	// r.HandleFunc("/api/posts/{id}", deletePost).Methods("DELETE")
 
 	http.HandleFunc("/admin/dashboard", dashboard.Dashboard)
+
+	http.Handle("/", r)
 }
