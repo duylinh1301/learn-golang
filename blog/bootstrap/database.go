45df3473b7c7e155bb9d6099a55d3166c9df2121
Duy Linh <linhnguyen@okxe.vn>
2020-12-16 18:27:39 +0700

    add GORM package

---
+++ blog/bootstrap/database.go
@@ -0,0 +1,15 @@
+package bootstrap
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func Connect() {
+	dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+}
+
+func SetupDatabase(config map[string]string) {
+
+}
